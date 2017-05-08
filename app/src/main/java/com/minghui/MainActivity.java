1242f7499cdd3a857fa3bc0d88a03bc5e6c25495
yq <648500318@qq.com>
2017-05-08 11:04:29 +0800

    send

--- app/src/main/java/com/minghui/MainActivity.java
+++ app/src/main/java/com/minghui/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,9 +14,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);//手机界面布局
     }
 
-
+    //点击事件四要素， public ,void ,点击事件方法名,View view
     public void sendMessage(View view) {
+        //Activity界面之间的交通枢纽,可以打包数据
         Intent intent = new Intent(this,MessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);//寻找视图
+        System.out.println(editText.getText());
+        intent.putExtra("address",editText.getText().toString());//地址：北京市朝阳区菜市口...
         startActivity(intent);
     }
 }
--- app/src/main/java/com/minghui/MessageActivity.java
+++ app/src/main/java/com/minghui/MessageActivity.java
@@ -1,7 +1,9 @@
 package com.minghui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MessageActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("address");//获取打包过来的值
+        System.out.println(str);
+        TextView tv = (TextView) findViewById(R.id.tv);
+        tv.setText(str);
+
     }
 }
